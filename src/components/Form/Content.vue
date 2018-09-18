e6207a52f2276232a0f923d4c9f56e1d62d010db
Thomas Urban <thomas.urban@cepharum.de>
2018-09-18 23:05:59 +0200

    reducing form components now using store attached to components

--- src/components/Form/Content.vue
+++ src/components/Form/Content.vue
@@ -1,17 +1,12 @@
-<template>
-  <div class="form-view">
-    <FormContentSequence />
-  </div>
-</template>
-
 <script>
-import Vue from "vue";
-import Store from "@/store";
-
 export default {
 	name: "FormContent",
-	components: {
-		FormContentSequence: resolve => resolve( Vue.component( "FormContentSequence", Store.getters.sequence.formsComponent ) ),
+	render( createElement ) {
+		return createElement( "div", {
+			class: "form-view",
+		}, [
+			createElement( this.$store.getters.sequence.formsComponent ),
+		] );
 	},
 };
 </script>
--- src/components/Form/Progress.vue
+++ src/components/Form/Progress.vue
@@ -1,19 +1,12 @@
-<template>
-  <div class="form-progress">
-    <FormProgressSequence />
-  </div>
-</template>
-
 <script>
-import Vue from "vue";
-import Store from "@/store";
-
 export default {
 	name: "FormProgress",
-	components: {
-		FormProgressSequence: resolve => {
-			resolve( Vue.component( "FormProgressSequence", Store.getters.sequence.progressComponent ) );
-		},
+	render( createElement ) {
+		return createElement( "div", {
+			class: "form-progress",
+		}, [
+			createElement( this.$store.getters.sequence.progressComponent ),
+		] );
 	},
 };
 </script>
