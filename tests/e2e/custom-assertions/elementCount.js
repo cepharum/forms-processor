26704927ad72744b46eed282eb2e8e4f06e05820
Thomas Urban <thomas.urban@cepharum.de>
2018-10-21 03:19:10 +0200

    fixing custom assertion not working due to using arrow function

--- tests/e2e/custom-assertions/elementCount.js
+++ tests/e2e/custom-assertions/elementCount.js
@@ -8,14 +8,20 @@
 // http://nightwatchjs.org/guide#writing-custom-assertions
 
 exports.assertion = function elementCount( selector, count ) {
-	this.message = `Testing if element <${selector}> has count: ${count}`;
+	this.message = `Testing if element <${selector}> exists ${count} ${count === 1 ? "time" : "times"}`;
 	this.expected = count;
 	this.pass = val => val === count;
 	this.value = res => res.value;
 
-	const evaluator = _selector => {
+	/**
+	 * Counts number of elements matching provided CSS selector.
+	 *
+	 * @param {string} _selector CSS selector
+	 * @return {number} number of elements matching provided CSS selector
+	 */
+	function evaluator( _selector ) {
 		return document.querySelectorAll( _selector ).length;
-	};
+	}
 
 	this.command = cb => this.api.execute( evaluator, [selector], cb );
 };
