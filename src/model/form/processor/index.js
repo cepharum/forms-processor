008ea09e320dd25b360d33e914592f02bc19e985
Thomas Urban <thomas.urban@cepharum.de>
2019-01-12 13:52:42 +0100

    adding informational comment

--- src/model/form/processor/index.js
+++ src/model/form/processor/index.js
@@ -33,6 +33,7 @@ import FormProcessorMapModel from "./map";
 export default {
 	abstract: FormProcessorAbstractModel,
 	map: {
+		// maps type name used in a forms' definition into the proper processor model
 		send: FormProcessorSendModel,
 		map: FormProcessorMapModel,
 	},
