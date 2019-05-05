6e41ef5659e5cf8dba3b32eae9bd2b2e30ffde59
Thomas Urban <thomas.urban@cepharum.de>
2019-05-05 11:14:46 +0200

    fixing E2E test

--- tests/e2e/specs/group/dependency-inside.js
+++ tests/e2e/specs/group/dependency-inside.js
@@ -9,6 +9,7 @@ module.exports = { "Group Field: contained field is updated on change of non-con
 		.assert.elementPresent( ".field.name-reported" )
 		.assert.elementCount( ".field.name-reported .field", 4 )
 
+		.clearValue( ".field.name-lastname input[type=text]" )
 		.setValue( ".field.name-lastname input[type=text]", "Doe" )
 		.pause( 100 )
 		.assert.matchesText( ".field.name-reported .field.name-unnamed0003 .static-info", /\bDoe\b/ )
